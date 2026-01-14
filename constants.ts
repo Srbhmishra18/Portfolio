6ee2fe0ede18c41f74cb33b291a0ddedec7a03b5
Saurabh Mishra <Srbhmishra18@gmail.com>
2026-01-14 01:03:08 +0100

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -272,7 +272,7 @@ export const PROJECTS: Project[] = [
     tech: ['PostgreSQL', 'Advanced SQL', 'Business Analytics', 'Data Modeling'],
     link: 'https://github.com/Srbhmishra18/Advanced-SQL-Business-Analytics-PostgreSQL',
     github: 'https://github.com/Srbhmishra18/Advanced-SQL-Business-Analytics-PostgreSQL',
-    image: 'https://images.unsplash.com/photo-1544383835-bda2bc66a55d?q=80&w=800&auto=format&fit=crop',
+    image: 'https://images.unsplash.com/photo-1526628953301-3e589a6a8b74?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     id: '3',
